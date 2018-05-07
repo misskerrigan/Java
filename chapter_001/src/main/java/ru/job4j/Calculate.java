f6fa011defb571b88286a57e0e87e0b4e566406b
misskerrigan <miss.kerrigan@yandex.ru>
2018-05-07 19:09:02 +0800

    1.3.1 Maven Plugins

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+/** 
+ * Calculate. 
+ * @author Anastacia Boiko (miss.kerrigan@yandex.ru)
+*/
+
+public class Calculate {
+/**
+ * Main.
+ * @param args - args.
+ */	
+		public static void main(String[] args) {
+			System.out.println("Hello World");
+		}
+		
+}
