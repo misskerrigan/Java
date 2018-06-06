5199f063ab7abd9e84006c9064216ed407f3154f
misskerrigan <miss.kerrigan@yandex.ru>
2018-06-06 17:38:32 +0800

    Counter edit3

--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -8,13 +8,8 @@ import static org.junit.Assert.assertThat;
 public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
-        int start = 0;
-        int finish = 10;
         Counter counter = new Counter();
         int result = counter.add(0, 10);
         assertThat(result, is(30));
-
-
-
     }
 }
