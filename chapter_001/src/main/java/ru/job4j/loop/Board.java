13141ef45e6fe5706dd60035a06962c0b33e6727
misskerrigan <miss.kerrigan@yandex.ru>
2018-06-12 20:40:24 +0800

    Board edit2

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -4,30 +4,11 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for (height = 1; height <= 3; height++) {
-            for (width = 1; width <= 3; width++) {
+        for (int i = 1; i <= height; i++) {
+            for (int bu = 1; bu <= width; bu++) {
                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
-                if ((height + width) % 2 == 0) {
-                    screen.append("X");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // добавляем перевод на новую строку.
-            screen.append(ln);
-        }
-        return screen.toString();
-    }
-
-    public String painttwo(int width, int height) {
-        StringBuilder screen = new StringBuilder();
-        String ln = System.lineSeparator();
-        for (height = 1; height <= 4; height++) {
-            for (width = 1; width <= 5; width++) {
-                // условие проверки, что писать пробел или X
-                // Выше в задании мы определили закономерность, когда нужно проставлять X
-                if ((height + width) % 2 == 0) {
+                if ((i + bu) % 2 == 0) {
                     screen.append("X");
                 } else {
                     screen.append(" ");
