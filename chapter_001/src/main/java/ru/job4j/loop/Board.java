301e64ace8466d0898145e3624502450cf954558
misskerrigan <miss.kerrigan@yandex.ru>
2018-06-12 20:14:57 +0800

    Board edit

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -19,4 +19,23 @@ public class Board {
         }
         return screen.toString();
     }
+
+    public String painttwo(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (height = 1; height <= 4; height++) {
+            for (width = 1; width <= 5; width++) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if ((height + width) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
 }
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -18,10 +18,10 @@ public class BoardTest {
         String ln = System.lineSeparator();
         assertThat(rsl, is(String.format("X X%s X %sX X%s", ln, ln, ln)));
     }
-
+    @Test
     public void when5x4() {
         Board board = new Board();
-        String rsl = board.paint(5, 4);
+        String rsl = board.painttwo(5, 4);
         String ln = System.lineSeparator();
         assertThat(rsl, is(String.format("X X X%s X X %sX X X%s X X %s", ln, ln, ln, ln)));
     }
