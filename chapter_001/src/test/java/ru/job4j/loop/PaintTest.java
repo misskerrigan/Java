0b6e9b13aefe0ab936dc3e5982dbe6ed50e3fa2c
misskerrigan <miss.kerrigan@yandex.ru>
2018-06-22 04:23:13 +0800

    Пирамида

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Anastacia Boiko (mailto:miss.kerrigan@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PaintTest {
+    @Test
+    public void whenPyramid4() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()));
+    }
+}
