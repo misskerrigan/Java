dc16dbf4e2f30b0af863849f18d7135ed3860f45
misskerrigan <miss.kerrigan@yandex.ru>
2018-06-24 02:46:58 +0800

    Пирамида. Рефакторинг кода.

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -12,6 +12,38 @@ import static org.junit.Assert.*;
  */
 public class PaintTest {
     @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
     public void whenPyramid4() {
         Paint paint = new Paint();
         String rst = paint.pyramid(4);
