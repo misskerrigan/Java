facbaad879a92d9d38cd381b873ad929164cbba2
misskerrigan <miss.kerrigan@yandex.ru>
2018-07-18 03:18:39 +0800

    Слова начинается с ...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i < value.length; i++) {
+            if (this.data[i] != value[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
