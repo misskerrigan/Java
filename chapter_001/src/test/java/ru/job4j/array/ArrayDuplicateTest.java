6a0edd455f279a2273d35509d4f9e731f9b6a57c
misskerrigan <miss.kerrigan@yandex.ru>
2018-07-31 03:11:21 +0800

    Удаление дубликатов в массиве(red)

--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -10,18 +10,18 @@ public class ArrayDuplicateTest {
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
         ArrayDuplicate duplicate = new ArrayDuplicate();
-        String [] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
-        String [] expect = {"Привет", "Мир", "Супер"};
-        String [] result = duplicate.remove(input);
+        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] expect = {"Привет", "Мир", "Супер"};
+        String[] result = duplicate.remove(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate2() {
         ArrayDuplicate duplicate = new ArrayDuplicate();
-        String [] input = {"а", "а", "б", "б", "в"};
-        String [] expect = {"а", "б", "в"};
-        String [] result = duplicate.remove(input);
+        String[] input = {"а", "а", "б", "б", "в"};
+        String[] expect = {"а", "б", "в"};
+        String[] result = duplicate.remove(input);
         assertThat(result, arrayContainingInAnyOrder(expect));
     }
 }
